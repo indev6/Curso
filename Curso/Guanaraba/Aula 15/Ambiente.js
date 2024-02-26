9fe061172d907d1686652391611dd009d967f258
indev6 <igor_neves@hotmail.com>
2024-02-26 13:33:06 -0300

    start

---
+++ Curso/Guanaraba/Aula 15/Ambiente.js
@@ -0,0 +1,12 @@
+let num = [5, 8, 2, 9, 3]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+
+
+//num.push() Adiciona um elemente a ultima posição do arrey
+//num.length - para saber quantos elementos tem on arrey
+//num.sort() = Coloca os elementos em ordem crescente
